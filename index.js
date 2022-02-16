6e2af081dba7128ce078b13c2ba0bad19ca4c880
Silvia Martinez <sil@silvia.com>
2022-02-16 22:34:21 +0100

    added style at html text

--- index.js
+++ index.js
@@ -8,36 +8,78 @@ const chalk = require("chalk");
 const calculadora = require("./calculadora");
 
 const server = http.createServer();
-const port = 3002;
+const port = process.env.SERVER_PORT;
 
 server.listen(port, () => {
-  debug(`Server is up at http://localhost:${port}`);
+  debug(chalk.green(`Server is up at http://localhost:${port}/calculadora/`));
 });
 server.on("error", (error) => {
-  debug(chalk.red(`Error on server ${port}: Error`));
+  debug(chalk.red(`Error on server ${port}: Error:${error.message}`));
 });
 
 server.on("request", (request, response) => {
-  debug(`Your request has arrived at ${port}`);
+  debug(chalk.yellow(`Your request has arrived at ${port}`));
 
   const query = url.parse(request.url, true).query;
   const a = query.a;
   const b = query.b;
 
+  if (Number.isNaN(a) || Number.isNaN(b)) {
+    response.setHeader("Content-type", "text/html");
+    response.statusCode = 401;
+    response.write(`<head>
+      <style>
+      body {background-color:grey;font-family:arial;}
+      h1   {color: orange; display: flex; justify-content: center;}
+      h2   {color: blue;}
+      div  {display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-content: center;
+    align-items: center;
+    justify-content: center;}
+      p    {color: black; font-size: 25px; margin:10px}
+      span {color: pink;}
+      </style>
+    </head>
+    <body>
+    <h1>Calculadora</h1>
+    <div ><h2 > Please insert numbers </h2></div>
+    </body>`);
+    response.end();
+  }
+
   response.setHeader("Content-type", "text/html");
   response.statusCode = 200;
   response.write(
-    `<h1>Calculadora</h1>
-    <div>
-    <h2> suma:</h2><p> ${a} + ${b} = ${
+    `
+    <head>
+      <style>
+      body {background-color:grey;font-family:arial;}
+      h1   {color: orange; display: flex; justify-content: center;}
+      h2   {color: blue;}
+      div  {display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-content: center;
+    align-items: center;
+    justify-content: center;}
+      p    {color: black; font-size: 25px; margin:10px}
+      span {color: pink;}
+      </style>
+    </head>
+    <body>
+    <h1>Calculadora</h1>
+    <div >
+    <h2 > suma: </h2><p> ${a} + ${b} =<span> ${
       Number(a) + Number(b)
-    }</p> </div><div><h2> resta:</h2><p> ${a} - ${b} = ${
+    }</span></p> </div> <div><h2> resta: </h2><p> ${a} - ${b} = <span>${
       Number(a) - Number(b)
-    }<div><h2> multiplicacion:</h2><p> ${a} x ${b} = ${
+    }</span></div><div><h2> multiplicacion: </h2><p> ${a} x ${b} = <span>${
       Number(a) * Number(b)
-    }</p> </div> <div><h2> divisionon:</h2><p> ${a} / ${b} = ${
+    }</span></p> </div> <div><h2> division: </h2><p> ${a} / ${b} = <span>${
       Number(a) / Number(b)
-    }</p> </div>
+    }</span></p> </div></body>
     `
   );
   response.end();
