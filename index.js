8507bb6a76d9d3f0e48118271641c0105e5a84d3
Silvia Martinez <sil@silvia.com>
2022-02-16 18:35:20 +0100

    Update file server

--- index.js
+++ index.js
@@ -6,24 +6,11 @@ const { program } = require("commander");
 const chalk = require("chalk");
 const calculadora = require("./calculadora");
 
-const server = http.createServer();
-server.listen(3002, () => {
-  debug("server is up");
-});
-
-prompt.start();
-program.option("-a --numberA <number>");
-program.option("-b --numberB <number>");
-program.parse();
-
-const askNumbers = async () => {
-  const { numberA, numberB } = await prompt.get(["numberA", "numberB"]);
-  const result = calculadora(numberA, numberB);
-
-  debug(chalk.green(`${numberA} + ${numberB} = ${result[0]}`));
-  debug(chalk.red(`${numberA} - ${numberB} = ${result[1]}`));
-  debug(chalk.yellow(`${numberA} * ${numberB} = ${result[2]}`));
-  debug(chalk.blue(`${numberA} / ${numberB} = ${result[3]}`));
+const requestListener = function (req, res) {
+  res.writeHead(200, { "Content-type": "text/html" });
+  res.write("Hello, World!");
+  res.end();
 };
 
-askNumbers();
+const server = http.createServer(requestListener);
+server.listen(3002);
