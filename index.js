1991a865590b81ed8d585eb08d0393b6f1bca8b2
Silvia Martinez <sil@silvia.com>
2022-02-16 19:25:06 +0100

    Update create server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const http = require("http");
+const url = require("url");
 const debug = require("debug")("calc");
 const prompt = require("prompt");
 const { program } = require("commander");
@@ -8,9 +9,10 @@ const calculadora = require("./calculadora");
 
 const requestListener = function (req, res) {
   res.writeHead(200, { "Content-type": "text/html" });
-  res.write("Hello, World!");
-  res.end();
-};
+  const query = url.parse(req.url, true).query;
+  const text = query.a + query.b;
 
+  res.end(text);
+};
 const server = http.createServer(requestListener);
 server.listen(3002);
